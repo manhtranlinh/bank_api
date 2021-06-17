5d5bca0d952b735251ac113e4f867cb5cd8454e8
Tran Manh Linh <manhtranlinh@gmail.com>
2021-06-17 15:33:49 +0700

    commit before change the the config/test.exs

--- lib/bank_api/command_router.ex
+++ lib/bank_api/command_router.ex
@@ -5,5 +5,7 @@ defmodule BankAPI.CommandRouter do
   alias BankAPI.Accounts.Aggregates.Account
   alias BankAPI.Accounts.Commands.OpenAccount
 
+  middleware BankAPI.Middleware.ValidateCommand
+  
   dispatch([OpenAccount], to: Account, identity: :account_uuid)
 end
--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule BankAPI.MixProject do
       {:commanded, "~> 1.2"},
       {:eventstore, "~> 1.3", runtime: Mix.env() != :test},
       {:commanded_eventstore_adapter, "~> 1.2", runtime: Mix.env() != :test},
-      {:commanded_ecto_projections, "~> 1.2"}
+      {:commanded_ecto_projections, "~> 1.2"},
+      {:skooma, "~> 0.2.0"}
     ]
   end
 
